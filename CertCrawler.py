de076ef8eee060078bc133418a8fda029ca5b802
Chillis&Associates_CEO <72903795+CypherHippie@users.noreply.github.com>
2024-04-10 21:21:03 -0600

    Signed-off-by: CypherHippie_COO <72903795+CypherHippie@users.noreply.github.com>

--- CertCrawler.py
+++ CertCrawler.py
@@ -2,8 +2,8 @@ import aiohttp
 import asyncio
 import time
 import logging
-from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+from lxml import html as lh
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -29,9 +29,9 @@ async def fetch(url):
 
 def get_internal_links(url, html_content):
     internal_links = set()
-    soup = BeautifulSoup(html_content, 'html.parser')
-    for link in soup.find_all('a', href=True):
-        next_url = urljoin(url, link['href'])
+    tree = lh.fromstring(html_content)
+    for link in tree.xpath('//a/@href'):
+        next_url = urljoin(url, link)
         parsed_next_url = urlparse(next_url)
         if parsed_next_url.scheme and parsed_next_url.netloc == urlparse(url).netloc:
             internal_links.add(next_url)
@@ -42,24 +42,13 @@ async def crawl(url, depth):
         visited_urls.add(url)
         html_content = await fetch(url)
         if html_content:
-            soup = BeautifulSoup(html_content, 'html.parser')
-            forms = soup.find_all('form')
-            input_texts = soup.find_all('input', {'type': 'text'})
+            tree = lh.fromstring(html_content)
+            forms = tree.xpath('//form')
+            input_texts = tree.xpath('//input[@type="text"]')
             if forms or input_texts:
                 logger.info(f"Form Found: {url}")
             internal_links = get_internal_links(url, html_content)
             await asyncio.gather(*[crawl(link, depth + 1) for link in internal_links])
-
-async def process_form_submission(url, form_data):
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.post(url, data=form_data, timeout=10) as response:
-                if response.status == 200:
-                    logger.info(f"Form submitted successfully: {url}")
-                else:
-                    logger.warning(f"Failed to submit form: {url}. Status code: {response.status}")
-        except Exception as e:
-            logger.error(f"Error submitting form: {url}. {e}")
 
 async def main():
     domain = input("Enter the domain you want to crawl (e.g., example.com): ")
